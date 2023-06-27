ee7c1bce5ba18febc5504c9de931dd109e9a07b7
yun <syusmm233@gmail.com>
2023-06-27 18:18:20 +0900

    1303

--- 1303.py
+++ 1303.py
@@ -1,113 +1,106 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#전투 bfs, dfs문제
-# 이 문제는 방문, 조회해서 푸는 문제구나. 
+# 전투 bfs, dfs문제
+# 이 문제는 방문, 조회해서 푸는 문제구나.
 # bfs로 풀면 될듯. dfs
-# 일단 bfs 코드 
+# 일단 bfs 코드
 # N = 5
 # M =5
 import sys
-sys.stdin = open('1303.txt','r')
+
+sys.stdin = open("1303.txt", "r")
 # N,M= map(int, input().split())
-N,M= map(int, input().split())
-print("N:",N)
-print("M:",M)
+N, M = map(int, input().split())
+print("N:", N)
+print("M:", M)
 
 from collections import deque
-# first = list(input().split())
-# rows = input.str.split('\n')
-grid =  [list(row) for row in sys.stdin.read().split()]
+
+grid = [list(row) for row in sys.stdin.read().split()]
+w_number = 0
+b_number = 0
 
 
-print("grid:",grid)
-print("grid[0][0]:",grid[0][0])
-def battle(grid):
-    ww = []
-    w_number = 0    
-    bb = []
-    b_number = 0
-    visited =[ [False]*(N) for _ in range(N)]
-    print("visited:",visited)
+def battle(x, y, color):
+    # 현재 병사의 수
+    count = 0
+    visited = [[False] * (N) for _ in range(N)]
+
     row = len(grid)
-    #인덱스 0이 살짝 헷갈림
+    # 인덱스 0이 살짝 헷갈림
     col = len(grid[0])
 
-    dx = [[1,0] ,[-1,0],[0,-1],[0,1]]
+    dx = [[1, 0], [-1, 0], [0, -1], [0, 1]]
 
     # f, s = dx[0]
     # print(f)
     # print(s)
 
-    #다음 방문할 곳
-    if grid[0][0] == 'w':
-         w_number = 1
-    else:
-        b_number = 1
-    q= deque()
-    q.append((0,0))
-    print("qqqq:",q)
-        #첫 시작 방문
+    # 다음 방문할 곳
+    # if grid[0][0] == "w":
+    #     w_number = 1
+    # else:
+    #     b_number = 1
+
+    q = deque()
+    q.append((0, 0))
+
+    # 첫 시작 방문
     visited[0][0] = True
-    print("q",q)
+
     while q:
-        #방문할 곳
-        cur_x,cur_y = q.popleft()
-        print("cur_x",cur_x)
-        print("cur_y",cur_y)
+        # 방문할 곳
+        cur_x, cur_y = q.popleft()
+        print("cur_x", cur_x)
+        print("cur_y", cur_y)
+
         for i in range(4):
             next_x = cur_x + dx[i][0]
-            next_y = cur_y + dx[0][i]
-            print("next_x:",next_x)
-            print("next_y:",next_y)
-            #w 가 w면 몇개인지 세야함. 배열에 저장해놔야함
-            #b가 b면 몇개인지 기록해야함. 배열에 저장해놔야함
-            if (next_x >= 0 and next_x < row and next_y >= 0 and next_y < col and visited[next_x][next_y] == False):
-                if grid[next_x][next_y] == 'W'  :
-                   
-                    w_number += 1
-                    # print("w_number:",w_number)
-                   
-                    # if next_x == 0 or next_y == 0 or grid[next_x][next_y] == 'B'
-                elif grid[next_x][next_y] == 'B'  :
-            
-                   
-                    b_number += 1
-                q.append((next_x,next_y))
-                visited[next_x][next_y] == True
-                print("w_number,b_number:",w_number,b_number)
-            return w_number,b_number
-                # elif next_x ==0 or next_x == 0 
-                
-
-
-
+            next_y = cur_y + dx[i][1]
+            print("dx[i][0]:", dx[i][0])
+            print("dx[0][i]:", dx[i][1])
+            print("next_x:", next_x)
+            print("next_y:", next_y)
+            # w 가 w면 몇개인지 세야함. 배열에 저장해놔야함
+            # b가 b면 몇개인지 기록해야함. 배열에 저장해놔야함
+            if (
+                next_x >= 0
+                and next_x < row
+                and next_y >= 0
+                and next_y < col
+                and visited[next_x][next_y] == False
+                and grid[next_x][next_y] == color  # 각 색을 어떻게 저장하나 했더니 color를 확인하는구나.
+            ):
+                # print("grid[next_x][next_y]:", grid[next_x][next_y])
+                # if grid[next_x][next_y] == "W":
                 #     w_number += 1
-                # elif  grid[next_x][next_x] != 'W' or :
-                #     ww.append(w_number)
-                #     print("ww:",ww)
-                # elif grid[next_x][next_x] == 'B':
+                #     print("")
+
+                #     # if next_x == 0 or next_y == 0 or grid[next_x][next_y] == 'B'
+                # elif grid[next_x][next_y] == "B":
                 #     b_number += 1
 
+                # 방문 기록 및 다음방문 저장
+                q.append((next_x, next_y))
+                visited[next_x][next_y] = True
+
+                print("visited:", visited)
+                count += 1
+    return count + 1  # 1을 더하는 이유가 뭐지? #맨처음 시작 1을 더하는건가?????
 
 
-    
+# 이제 각 색의 값을 제곱해서 더해야함
 
-        return
-        #방문했다는걸 기록해야함.
-        if x not in visited:
-            visited
-        #다음에 방문할 곳을 추가해야함!
-        #동서남북으로 이동 next_q
-        #x의 현위치를 어떻게 파악하지?
-        #grid로! ㅎ
-        # for r in
-        #     next_q = x + dx[r]
-        #     if x 
-        # q.append()
+# if grid[next_x][next_y] == "W":
+#     w_number += 1
+#     print("")
+
+#     # if next_x == 0 or next_y == 0 or grid[next_x][next_y] == 'B'
+# elif grid[next_x][next_y] == "B":
+#     b_number += 1
 
 
-
-print("bfs::",battle(grid))
+print("bfs::", battle(grid))
 
 # grid = [ [-1]* (N) for _ in range(M)]
 # print("grid:",grid)
@@ -116,5 +109,3 @@ print("bfs::",battle(grid))
 # def bfs():
 #     q = deque()
 #     q.append((0,0))
-
-
